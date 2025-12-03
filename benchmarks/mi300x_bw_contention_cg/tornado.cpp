75a46cd51a81ae63dae0464015061266a500337e
gajagajago <gajagajago@naver.com>
2025-12-03 15:24:27 -0600

    everything

--- benchmarks/mi300x_bw_contention_cg/tornado.cpp
+++ benchmarks/mi300x_bw_contention_cg/tornado.cpp
@@ -1,4 +1,4 @@
-// all xcds generate local iod traffic
+// all xcds generate tornado iod traffic
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,6 +16,10 @@
 
 #ifndef XCDS_NUM
 #define XCDS_NUM 8
+#endif
+
+#ifndef XTO
+#define XTO 2 // xcd tornado offset. 2 = 1 iod hop, 4 = 2 iod hops
 #endif
 
 using namespace std;
@@ -90,11 +94,14 @@ __global__ void k(uint64_t **xcd_chunks, size_t *xcd_chunks_size, uint32_t **cyc
         #endif
     }
 
+    // set dst as tornado
+    const uint32_t xcd_dst = (xcc_id + XTO) % XCDS_NUM;
+
     // warmup
     // only 1 thread per xcd warms up entirely
     if (tbid_in_xcd == 0 && tid == 0) {
-        for (size_t i = 0; i < xcd_chunks_size[xcc_id]; i++) {
-            uint64_t ptr = xcd_chunks[xcc_id][i];
+        for (size_t i = 0; i < xcd_chunks_size[xcd_dst]; i++) {
+            uint64_t ptr = xcd_chunks[xcd_dst][i];
             uint4 *data_ptr = (uint4*)ptr;
             for (size_t offset = 0; offset < (CHUNK_SIZE / sizeof(uint4)); offset++) {
                 asm volatile (
@@ -108,11 +115,11 @@ __global__ void k(uint64_t **xcd_chunks, size_t *xcd_chunks_size, uint32_t **cyc
         }
     }
     xcd_barrier(xcc_id, n_tbs_in_xcd); // sync xcd
-    
+
     grid.sync(); // sync all xcds
 
     // measurement
-    size_t n_iter = xcd_chunks_size[xcc_id] / n_tbs_in_xcd;
+    size_t n_iter = xcd_chunks_size[xcd_dst] / n_tbs_in_xcd;
     if (tid == 0) {
         #if DEBUG
         printf("bid %d (tbid_in_xcd %d) on xcc %d: n_iter %zu\n", bid, tbid_in_xcd, xcc_id, n_iter);
@@ -126,7 +133,7 @@ __global__ void k(uint64_t **xcd_chunks, size_t *xcd_chunks_size, uint32_t **cyc
 
     for (size_t iter = 0; iter < n_iter; iter++) {
         size_t chunk_idx = tbid_in_xcd + iter * n_tbs_in_xcd;
-        uint64_t ptr = xcd_chunks[xcc_id][chunk_idx];
+        uint64_t ptr = xcd_chunks[xcd_dst][chunk_idx];
         uint4 *data_ptr = (uint4*)ptr;
         
         // see tid 0 cycle counts for debug
@@ -173,7 +180,17 @@ __global__ void k(uint64_t **xcd_chunks, size_t *xcd_chunks_size, uint32_t **cyc
 
 
 int main() {
-    printf("local\n");
+    switch (XTO) {
+        case 2:
+            printf("tornado traffic (1 hop)\n");
+            break;
+        case 4:
+            printf("tornado traffic (2 hops)\n");
+            break;
+        default:
+            printf("invalid XTO %d\n", XTO);
+            exit(1);
+    }
 
     const int n_threads_per_warp = 64;
     const int n_warps_per_block = (CHUNK_SIZE / (n_threads_per_warp * 16)); // set to make each tb load 1 chunk per iter. each thread loads 16B per iter.
@@ -231,8 +248,7 @@ int main() {
 
     hipLaunchKernelGGL(
         identify_home,
-        // dim3(n_blocks), dim3(n_threads_per_block), 
-        8, 128,
+        dim3(n_blocks), dim3(n_threads_per_block), 
         0, stream,
         (void*)d_data, data_size, d_cycles, d_home
     );
@@ -241,13 +257,11 @@ int main() {
     /* retrieve and process results */
     
     vector<vector<uint32_t>> h_cycles(XCDS_NUM, vector<uint32_t>(n_chunks));
-    // uint32_t h_cycles[XCDS_NUM][n_chunks]; // troubleshooting: stack overflow!
     for (int x = 0; x < XCDS_NUM; x++) {
         gpuErrchk(hipMemcpy(h_cycles[x].data(), d_cycles[x], sizeof(uint32_t) * n_chunks, hipMemcpyDeviceToHost));
     }
     
     vector<int> h_home(n_chunks);
-    // int h_home[n_chunks]; // troubleshooting: stack overflow!
     for (size_t i = 0; i < n_chunks; i++) {
         uint32_t min_cycles = 0xFFFFFFFF;
         int min_xcc = -1;
@@ -343,6 +357,9 @@ int main() {
         uint32_t min_xcd_cycles_start = 0xFFFFFFFF;
         uint32_t max_xcd_cycles_stop = 0;
 
+        // set dst xcd as tornado
+        const uint32_t xcd_dst = (xcd + XTO) % XCDS_NUM;
+
         for (int tbid_in_xcd = 0; tbid_in_xcd < BPX; tbid_in_xcd++) {
             // incremental avg
             avg_xcd_cycles += ((h_cycles_stop[xcd][tbid_in_xcd] - h_cycles_start[xcd][tbid_in_xcd]) - avg_xcd_cycles) / (tbid_in_xcd + 1);
@@ -360,7 +377,7 @@ int main() {
             {
                 // log per-tb results
                 uint32_t cycles = h_cycles_stop[xcd][tbid_in_xcd] - h_cycles_start[xcd][tbid_in_xcd];
-                double bytes = (double)((xcd_chunks_size[xcd] / BPX) * CHUNK_SIZE); // total bytes accessed by this tb
+                double bytes = (double)((xcd_chunks_size[xcd_dst] / BPX) * CHUNK_SIZE); // total bytes accessed by this tb
                 double cycles_per_load = (double)cycles / (bytes / n_threads_per_block / 16.0); // per 16B load
                 printf("xcd %d, tb %d: start %u, stop %u, lat(16B) %.2f cycles\n", 
                         xcd, tbid_in_xcd, h_cycles_start[xcd][tbid_in_xcd], h_cycles_stop[xcd][tbid_in_xcd], cycles_per_load);
@@ -369,7 +386,7 @@ int main() {
         }
 
         // avg. latency (RTT)
-        const double bytes = (double)(xcd_chunks_size[xcd] * CHUNK_SIZE); // total bytes accessed by this xcd
+        const double bytes = (double)(xcd_chunks_size[xcd_dst] * CHUNK_SIZE); // total bytes accessed by this xcd
         const double lat_cycles_16B = avg_xcd_cycles / (bytes / (n_threads_per_block * BPX) / 16.0);  // avg cycles per 16B load
         printf("xcd %d: avg lat(16B) %.2f cycles\n", xcd, lat_cycles_16B);
 
@@ -389,7 +406,7 @@ int main() {
             for (int tbid_in_xcd = 0; tbid_in_xcd < BPX; tbid_in_xcd++) {
                 if ( (t >= h_cycles_start[xcd][tbid_in_xcd]) && (t < h_cycles_stop[xcd][tbid_in_xcd]) ) {
                     // increment bytes by the approx. proportion loaded in this window
-                    double bytes_contrib = (double)((xcd_chunks_size[xcd] / BPX) * CHUNK_SIZE) * \
+                    double bytes_contrib = (double)((xcd_chunks_size[xcd_dst] / BPX) * CHUNK_SIZE) * \
                                         (window / (h_cycles_stop[xcd][tbid_in_xcd] - h_cycles_start[xcd][tbid_in_xcd]));
                     window_bytes += bytes_contrib;
                 }
