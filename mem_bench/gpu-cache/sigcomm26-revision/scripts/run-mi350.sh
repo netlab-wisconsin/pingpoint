aaba30c76c4d0b1a56f5c2064fd8081cbc9efb44
gajagajago <gajagajago@naver.com>
2026-06-02 15:43:01 -0500

    acn loaded latency for gpu-cache

---
+++ mem_bench/gpu-cache/sigcomm26-revision/scripts/run-mi350.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ARCH="mi350"
+
+SUFFIX="L1"
+
+HIP_VISIBLE_DEVICES=0 ../bin/acn-main-hop0-${ARCH}-${SUFFIX} 2>&1 | tee ../results-loaded-lat/acn-main-hop0-${ARCH}-${SUFFIX}.out &
+HIP_VISIBLE_DEVICES=1 ../bin/acn-main-hop1-${ARCH}-${SUFFIX} 2>&1 | tee ../results-loaded-lat/acn-main-hop1-${ARCH}-${SUFFIX}.out &
+
+SUFFIX="L2"
+
+HIP_VISIBLE_DEVICES=2 ../bin/acn-main-hop0-${ARCH}-${SUFFIX} 2>&1 | tee ../results-loaded-lat/acn-main-hop0-${ARCH}-${SUFFIX}.out &
+HIP_VISIBLE_DEVICES=3 ../bin/acn-main-hop1-${ARCH}-${SUFFIX} 2>&1 | tee ../results-loaded-lat/acn-main-hop1-${ARCH}-${SUFFIX}.out &
+
+SUFFIX="LLC"
+
+HIP_VISIBLE_DEVICES=4 ../bin/acn-main-hop0-${ARCH}-${SUFFIX} 2>&1 | tee ../results-loaded-lat/acn-main-hop0-${ARCH}-${SUFFIX}.out &
+HIP_VISIBLE_DEVICES=5 ../bin/acn-main-hop1-${ARCH}-${SUFFIX} 2>&1 | tee ../results-loaded-lat/acn-main-hop1-${ARCH}-${SUFFIX}.out &
+
+wait
